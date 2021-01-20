dc9ffcdfe787875f6ce264700d605b99aa9265e5
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-01-20 13:40:35 +0200

    chore(mocks): add products

---
+++ src/mocks/products.ts
@@ -0,0 +1,55 @@
+import { Product } from 'types';
+
+export const products1: Product[] = [
+  {
+    img: 'images/product-1.jpg',
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+  },
+  {
+    img: 'images/product-2.jpg',
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$16.99',
+  },
+  {
+    img: 'images/product-3.jpg',
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$14.99',
+  }
+];
+
+export const products2: Product[] = [
+  {
+    img: 'images/sweet-1.jpg',
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '$9.99',
+  },
+  {
+    img: 'images/sweet-2.jpg',
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '$12.99',
+  },
+  {
+    img: 'images/sweet-3.jpg',
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+  }
+];
