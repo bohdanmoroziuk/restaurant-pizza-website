972eac0d238e25cfbbd7ecdf052f33af1b9ea0d9
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-01-20 13:44:37 +0200

    feat: display homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { FC } from 'react';
+import GlobalStyle from 'GlobalStyle';
+import Hero from 'components/Hero';
+import Products from 'components/Products';
+import Feature from 'components/Feature';
+import Footer from 'components/Footer';
+import { products1, products2 } from 'mocks/products';
 
 const App: FC = () => (
-  <div className="app"></div>
+  <>
+    <GlobalStyle />
+    <Hero />
+    <Products title="Choose your favorite" products={products1} />
+    <Feature />
+    <Products title="Sweet Treats for You" products={products2} />
+    <Footer />
+  </>
 );
 
 export default App;
